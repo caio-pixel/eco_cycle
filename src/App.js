f49f8ac43fce902c657b449e50f80e957cbb410e
Kaique Almeida <kaique.batista@aluno.ifsp.edu.br>
2024-10-01 07:43:27 -0300

    Co-authored-by: caio-pixel <caio-pixel@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Create from "./components/createcheck"; // Importe o componente Create
+import Home from "./components/inicialpage/Home"; // Importe o componente Home
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Create />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/inicialpage/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./inicial.css"
+
+const Home = () => {
+  return (
+    <body className="inic">
+    <div>
+      <h1>Bem-vindo à Home!</h1>
+      <p>Você criou sua conta com sucesso.</p>
+    </div>
+    </body>
+  );
+};
+
+export default Home;
